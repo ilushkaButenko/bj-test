48ca0beea2372e6ba929d3116f1d8991dd1db20c
iButenko <ilabutenko256@gmail.com>
2021-02-13 12:12:39 +0300

    Created autoloader

--- App/Boot/Autoloader.php
+++ App/Boot/Autoloader.php
@@ -10,9 +10,40 @@ class Autoloader
 {
     public static function autoload()
     {
-        // find all in all directories
-        // all php files in
-        $files = scandir(ROOT_DIR);
+        // Load config
+        self::loadAllPhpFromDirectory(ROOT_DIR.DS.'App'.DS.'Config');
 
+        // Load other
+        foreach ([
+            APP_DIR,
+            CONTROLLERS_DIR,
+            MODELS_DIR,
+        ] as $path) {
+            self::loadAllPhpFromDirectory($path);
+        }
+    }
+
+    public static function isPhpFile($fileName)
+    {
+        return preg_match('/^.+\.php$/', $fileName) === 0 ? false : true;
+    }
+
+    public static function loadAllPhpFromDirectory($directoryPath)
+    {
+        $files = scandir($directoryPath);
+        $directoryPath = self::endWithSlash($directoryPath);
+        foreach ($files as $file) {
+            if (is_file($directoryPath.$file) && self::isPhpFile($file)) {
+                require_once($directoryPath.$file);
+            }
+        }
+    }
+
+    public static function endWithSlash($directoryPath)
+    {
+        if ($directoryPath[strlen($directoryPath)-1] !== DS) {
+            $directoryPath .= DS;
+        }
+        return $directoryPath;
     }
 }
--- App/Config/Directories.php
+++ App/Config/Directories.php
@@ -6,4 +6,10 @@
  */
 
 // Path to controllers directory
-define('CONTROLLERS_DIR', ROOT_DIR.DIRECTORY_SEPARATOR.'Controllers');
+define('CONTROLLERS_DIR', ROOT_DIR.DS.'Controllers');
+
+// Path to App directory
+define('APP_DIR', ROOT_DIR.DS.'App');
+
+// Path to models directory
+define('MODELS_DIR', ROOT_DIR.DS.'Models');
