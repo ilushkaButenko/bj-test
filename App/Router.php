82b4eef2b4276f5031dae9e2efeb947a52fa752c
iButenko <ilabutenko256@gmail.com>
2021-02-13 22:21:03 +0300

    Complete router logic.

--- App/Router.php
+++ App/Router.php
@@ -3,13 +3,58 @@
 namespace iButenko\App;
 
 /**
- * - App routing: getting controller class and method
+ * Router
+ * 
+ * Search requested controller class and method.
  */
 class Router
 {
+    const controllerPostfix = 'Controller';
+    const controllerPrefix = 'iButenko\Controllers\\';
+    const defaultMethodName = 'index';
+    const notFoundControllerName = 'NotFound';
+    
     private $controllerName = '';
     private $methodName = '';
     private $argument = '';
+    private $controllerClassName = '';
+    
+    /**
+     * findRealRoute
+     * 
+     * Finds out controller class and method. If controller or method
+     * doesn't exist it looks for suitable controller and method replace.
+     *
+     * @param  mixed $uri
+     * @param  mixed $baseUri
+     * @return void
+     */
+    public function findRealRoute($uri, $baseUri)
+    {
+        // Parse uri
+        $this->parseUri($uri, $baseUri);
+
+        $this->controllerClassName = self::controllerNameToClassName($this->controllerName);
+
+        if (!class_exists($this->controllerClassName)) {
+            $this->setNotFound();
+        }
+
+        if ($this->methodName === '') {
+            $this->methodName = self::defaultMethodName;
+        }
+
+        if (!method_exists($this->controllerClassName, $this->methodName)) {
+            $this->setNotFound();
+        }
+    }
+
+    public function setNotFound()
+    {
+        $this->controllerName = self::notFoundControllerName;
+        $this->methodName = self::defaultMethodName;
+        $this->controllerClassName = self::controllerNameToClassName($this->controllerName);
+    }
 
     public function parseUri($uri, $baseUri)
     {
@@ -40,5 +85,18 @@ class Router
     public function getArgument()
     {
         return $this->argument;
+    }
+
+    public function getControllerClassName()
+    {
+        return $this->controllerClassName;
+    }
+
+    /**
+     * Transform given controller name to full class controller name
+     */
+    public static function controllerNameToClassName($controllerName)
+    {
+        return self::controllerPrefix.ucfirst($controllerName).self::controllerPostfix;
     }
 }
