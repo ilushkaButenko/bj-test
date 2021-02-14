ff9cee1d070c37c050ddc56f5060fa2f04a4fee4
iButenko <ilabutenko256@gmail.com>
2021-02-14 22:50:05 +0300

    Added required data to task model and created method getTaskListPaginate

--- Models/Task.php
+++ Models/Task.php
@@ -3,11 +3,30 @@
 namespace iButenko\Models;
 
 use iButenko\App\Model;
+use iButenko\App\App;
 
 /**
  * Task model
  */
 class Task extends Model
 {
+    protected $tableName = 'tasks';
+    protected $columnNames = [
+        'name',
+        'email',
+        'task',
+        'done',
+    ];
 
+    public static function getTaskListPaginate($countPerPage, $pageNum = 1)
+    {
+        $pdo = App::getInstance()->getDatabase();
+
+        $sql = 'SELECT (name, email, task) FROM tasks ORDER BY id DESC LIMIT ?, ?';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            ($pageNum - 1) * $countPerPage,
+            $countPerPage
+        ]);
+    }
 }
