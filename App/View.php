214c8dee0acd0076e4d5166d2ca6225f9a60e5a7
iButenko <ilabutenko256@gmail.com>
2021-02-14 22:48:24 +0300

    Added ability to pass variables to views.

--- App/View.php
+++ App/View.php
@@ -12,13 +12,27 @@ class View
 {
     const headerFileName = VIEWS_DIR.DS.'parts'.DS.'header.php';
     const footerFileName = VIEWS_DIR.DS.'parts'.DS.'footer.php';
-
-    public static function render($view)
+    
+    /**
+     * render
+     *
+     * @param  mixed $view Name of php file in Views directory
+     * @param  mixed $data Associative array 'variable name' => 'variable value'
+     * @return void
+     */
+    public static function render($view, $data = null)
     {
         $viewFileName = VIEWS_DIR.DS.$view.'.php';
 
         if (!file_exists($viewFileName)) {
             throw new Exception('Template file '.$viewFileName.' was not found');
+        }
+
+        // Create variables
+        if (isset($data)) {
+            foreach ($data as $variableName => $variableValue) {
+                $$variableName = $variableValue;
+            }
         }
 
         require(self::headerFileName);
