3e60958d7ae46290b21bec85ba403f8fd0a15552
iButenko <ilabutenko256@gmail.com>
2021-02-14 21:47:34 +0300

    Created template parts. Added bootstrap and some basics.

--- App/View.php
+++ App/View.php
@@ -10,15 +10,19 @@ use PHPUnit\TextUI\TestFileNotFoundException;
  */
 class View
 {
+    const headerFileName = VIEWS_DIR.DS.'parts'.DS.'header.php';
+    const footerFileName = VIEWS_DIR.DS.'parts'.DS.'footer.php';
+
     public static function render($view)
     {
         $viewFileName = VIEWS_DIR.DS.$view.'.php';
 
-        if (file_exists($viewFileName)) {
-            require($viewFileName);
-        } else {
+        if (!file_exists($viewFileName)) {
             throw new Exception('Template file '.$viewFileName.' was not found');
         }
-        
+
+        require(self::headerFileName);
+        require($viewFileName);
+        require(self::footerFileName);
     }
 }
---
+++ Views/parts/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <base href="<?php echo BASE_URI ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>iButenko - Tasks</title>
+    <link href="/static/css/bootstrap.min.css" rel="stylesheet">
+    <script src="/static/js/bootstrap.min.js" type="text/javascript"></script>
+</head>
+<body>
+<div class="container">
