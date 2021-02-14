c216c7749e55c5a764c9a8054c3d409fca3577b0
iButenko <ilabutenko256@gmail.com>
2021-02-15 01:47:43 +0300

    Finished create task view.

--- Views/task-create.php
+++ Views/task-create.php
@@ -1,19 +1,46 @@
-<form method="POST" action="/task/store">
+<form method="POST" action="/task/create" class="needs-validation" novalidate>
 <div class="mb-3">
     <label for="name" class="form-label">Name</label>
-    <input id="name" class="form-control" aria-describedby="nameHelp" type="text" name="name" placeholder="Ilya Denisovich" pattern="[A-Z][a-z]+(\s[A-Z][a-z]+)?" required value="<?php echo $oldInput['name'] ?>">
+    <input id="name" class="form-control <?php if ($errors['name']) echo 'is-invalid'; elseif ($errors['name'] === false) echo 'is-valid' ?>" aria-describedby="nameHelp" type="text" name="name" placeholder="Ilya Denisovich" pattern="[A-Z][a-z]+(\s[A-Z][a-z]+)?" required value="<?php echo $oldInput['name'] ?>">
+    <?php if ($errors['name']): ?>
+    <div class="invalid-feedback">
+      <?php echo $errors['name'] ?>
+    </div>
+    <?php elseif ($errors['name'] === false): ?>
+    <div class="valid-feedback">
+      Ok!
+    </div>
+    <?php endif; ?>
     <div id="nameHelp" class="form-text">Please enter only letters and start with capital letter</div>
 </div>
 
 
 <div class="mb-3">
     <label for="email" class="form-label">Email</label>
-    <input id="email" class="form-control" type="email" name="email" placeholder="ilabutenko256@gmail.com" required value="<?php echo $oldInput['email'] ?>">
+    <input id="email" class="form-control <?php if ($errors['email']) echo 'is-invalid'; elseif ($errors['email'] === false) echo 'is-valid' ?>" type="email" name="email" placeholder="ilabutenko256@gmail.com" required value="<?php echo $oldInput['email'] ?>">
+    <?php if ($errors['email']): ?>
+    <div class="invalid-feedback">
+      <?php echo $errors['email'] ?>
+    </div>
+    <?php elseif ($errors['email'] === false): ?>
+    <div class="valid-feedback">
+      Ok!
+    </div>
+    <?php endif; ?>
 </div>
 
 <div class="mb-3">
     <label for="task" class="form-label">Task</label>
-    <textarea id="task" class="form-control" name="task" placeholder="Get a job at BeeJee" required value="<?php echo $oldInput['task'] ?>"></textarea>
+    <textarea id="task" class="form-control <?php if ($errors['task']) echo 'is-invalid'; elseif ($errors['task'] === false) echo 'is-valid' ?>" name="task" placeholder="Get a job at BeeJee" required><?php echo $oldInput['task'] ?></textarea>
+    <?php if ($errors['task']): ?>
+    <div class="invalid-feedback">
+      <?php echo $errors['task'] ?>
+    </div>
+    <?php elseif ($errors['task'] === false): ?>
+    <div class="valid-feedback">
+      Ok!
+    </div>
+    <?php endif; ?>
 </div>
 
 <input type="hidden" name="csrf" value="">
