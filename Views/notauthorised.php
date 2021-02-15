874576565aaae61b85077fe1bae8892b3d938007
iButenko <ilabutenko256@gmail.com>
2021-02-16 00:11:00 +0300

    Created non authorised page.

---
+++ Views/notauthorised.php
@@ -0,0 +1,11 @@
+<div class="alert alert-danger" role="alert">
+    <h4 class="alert-heading">You are not authorised</h4>
+    <p>You need permission to perform this action. You must login.</p>
+    <hr>
+    <div class="row">
+        <div class="col">
+            <a class="btn btn-primary" href="login" role="button">Login</a>
+            <a class="btn btn-primary" href="task" role="button">Cancel</a>
+        </div>
+    </div>
+</div>
