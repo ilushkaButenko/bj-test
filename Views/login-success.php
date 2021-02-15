4ff37aa2e1b938c490900e2896279038b9cd960e
iButenko <ilabutenko256@gmail.com>
2021-02-16 00:12:26 +0300

    Created notification pages.

---
+++ Views/login-success.php
@@ -0,0 +1,11 @@
+<div class="alert alert-success" role="alert">
+  <h4 class="alert-heading">You have successful logged in!</h4>
+  <p>Now you can continue working with task list as administrator. You can edit tasks, mark them done and delete.</p>
+  <hr>
+  <div class="row">
+  <div class="col">
+    <a class="btn btn-primary" href="/task" role="button">Continue</a>
+</div>
+    
+  </div>
+</div>
