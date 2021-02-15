5035c8d1b6d2bd6550a50b408539580351f408f1
iButenko <ilabutenko256@gmail.com>
2021-02-16 00:10:34 +0300

    Modified not found page.

--- Views/notfound.php
+++ Views/notfound.php
@@ -1,1 +1,10 @@
-<h1>Page not found</h1>
+<div class="alert alert-danger" role="alert">
+    <h4 class="alert-heading">Http error 404: Not found</h4>
+    <p>Page or resource you requested was not found</p>
+    <hr>
+    <div class="row">
+        <div class="col">
+            <a class="btn btn-primary" href="task" role="button">Main page</a>
+        </div>
+    </div>
+</div>
