d3ff6da996f1f42f5d41c612fdcd4f6b04d7a483
iButenko <ilabutenko256@gmail.com>
2021-02-16 04:03:21 +0300

    Created task create/edit notification.

---
+++ Views/task-edit-success.php
@@ -0,0 +1,35 @@
+<div class="alert alert-success" role="alert">
+<?php if ($createMode): ?>
+    <h4 class="alert-heading">Task has been created!</h4>
+    <p>New task looks like:</p>
+<?php else: ?>
+    <h4 class="alert-heading">Task has been edited!</h4>
+    <p>Now it looks like:</p>
+<?php endif ?>
+    
+    
+    <table class="task-table table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Task</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><?php echo $task['name'] ?></td>
+                <td><?php echo $task['email'] ?></td>
+                <td><?php echo $task['task'] ?></td>
+            </tr>
+        </tbody>
+    </table>
+
+
+    <hr>
+    <div class="row">
+    <div class="col">
+    <a class="btn btn-primary" href="/task" role="button">Continue</a>
+    </div>
+    </div>
+</div>
