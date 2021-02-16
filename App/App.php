2eb3d16e8ae5d3217304f7349e269978236804ae
iButenko <ilabutenko256@gmail.com>
2021-02-16 04:20:28 +0300

    Phpdoc.

--- App/App.php
+++ App/App.php
@@ -6,6 +6,8 @@ use iButenko\App\Router;
 
 /**
  * App
+ * 
+ * Singleton class representing an app.
  */
 class App
 {
@@ -50,37 +52,82 @@ class App
         $controller = new $className($this->router->getArgument());
         $controller->{$this->router->getMethodName()}();
     }
-
+    
+    /**
+     * connectToDatabase
+     *
+     * @return void
+     */
     private function connectToDatabase()
     {
         $this->db = new \PDO('mysql:dbname='.DB_NAME.';host='.DB_HOST, DB_USER, DB_PASS);
     }
-
+    
+    /**
+     * getDatabase
+     *
+     * @return PDO
+     */
     public function getDatabase(): \PDO
     {
         return $this->db;
     }
-
+    
+    /**
+     * getRouter
+     *
+     * @return Router
+     */
     public function getRouter(): Router
     {
         return $this->router;
     }
-
+    
+    /**
+     * redirect
+     * 
+     * Changes header location, substitudes base uri.
+     *
+     * @param  mixed $uri uri to go
+     * @param  mixed $code http code
+     * @return void
+     */
     public function redirect($uri, $code = 301)
     {
         header('Location: ' . BASE_URI . $uri, true, $code);
     }
-
+    
+    /**
+     * setStatusNotFound
+     * 
+     * Sets header 404
+     *
+     * @return void
+     */
     public function setStatusNotFound()
     {
         header('HTTP/1.0 404', true, 404);
     }
-
+    
+    /**
+     * setStatusForbidden
+     * 
+     * Sets 403 header
+     *
+     * @return void
+     */
     public function setStatusForbidden()
     {
         header('HTTP/1.0 403', true, 403);
     }
-
+    
+    /**
+     * setStatusError
+     * 
+     * Sets 500 header
+     *
+     * @return void
+     */
     public function setStatusError()
     {
         header('HTTP/1.0 500', true, 500);
