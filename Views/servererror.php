5d9d04574bc0b69bafbf5f3f87a644b71fbb466a
iButenko <ilabutenko256@gmail.com>
2021-02-16 04:02:23 +0300

    Created 500 error view.

---
+++ Views/servererror.php
@@ -0,0 +1,10 @@
+<div class="alert alert-danger" role="alert">
+    <h4 class="alert-heading">Http error 500: Internal server error</h4>
+    <p>Something has happened with server and this page cannot be shown. Please, try later.</p>
+    <hr>
+    <div class="row">
+        <div class="col">
+            <a class="btn btn-primary" href="task" role="button">Main page</a>
+        </div>
+    </div>
+</div>
