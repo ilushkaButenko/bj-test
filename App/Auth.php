53e17cb392720851c7704188344307102dfee740
iButenko <ilabutenko256@gmail.com>
2021-02-16 03:52:46 +0300

    Created Auth class.

---
+++ App/Auth.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace iButenko\App;
+
+class Auth
+{
+    private static $expectedPassword = '$2y$10$ADPmb8BBtJp2GFI.qfaYwufEzf6Iv8KthUyUOfmfb4qoApxZNMLwW';
+    
+    /**
+     * check
+     *
+     * @param  boolean $showError throw 403 error if not authorised
+     * @return boolean|void
+     */
+    public static function check($showError = true)
+    {
+        if ($showError && $_SESSION['auth'] !== true) {
+            App::getInstance()->setStatusForbidden();
+            View::render('notauthorised');
+            exit();
+        }
+        return $_SESSION['auth'] === true;
+    }
+
+    public static function login($login, $password)
+    {
+        if ($login === 'admin' && password_verify($password, self::$expectedPassword)) {
+            $_SESSION['auth'] = true;
+            return true;
+        }
+        $_SESSION['auth'] = false;
+        return false;
+    }
+
+    public static function logout()
+    {
+        $_SESSION['auth'] = false;
+    }
+}
